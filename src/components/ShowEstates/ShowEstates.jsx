790b472765d2bd8363de2233157b8f8b0172aefc
Nujat-Zaman-Chowdhury <nujattazin@gmail.com>
2024-04-10 20:30:02 +0600

    commited

--- src/components/ShowEstates/ShowEstates.jsx
+++ src/components/ShowEstates/ShowEstates.jsx
@@ -17,7 +17,7 @@ const ShowEstates = () => {
             <h3 className="text-4xl font-roboto text-[#2B262D] font-semibold">Latest Properties</h3>
             <p className="text-xl text-grey-500">We provide the best property you like</p>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4" >
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4" >
                 {
                     allData.map(data=><SingleEstates key={data.id} data={data}></SingleEstates>)
                 }
