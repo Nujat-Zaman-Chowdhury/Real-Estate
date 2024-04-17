97eef5a9856e5949e766aa9dca474f9fc326a0f5
Nujat-Zaman-Chowdhury <nujattazin@gmail.com>
2024-04-17 09:57:03 +0600

    added some changes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,19 +2,19 @@ import { Marker, Popup } from 'react-leaflet';
 import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
 import 'Leaflet/dist/leaflet.css'
-import gpsIcon from "../../assets/gps.png";
+// import gpsIcon from "../../assets/gps.png";
 import { FaMobile} from 'react-icons/fa';
 import { FaMapLocation } from 'react-icons/fa6';
 import { IoMdMail } from 'react-icons/io';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
-import { Icon } from 'leaflet';
+// import { Icon } from 'leaflet';
 const position = [51.505, -0.08]
 const Contact = () => {
-    const customIcon = new Icon({
+    // const customIcon = new Icon({
         
-        iconUrl: gpsIcon,
-        iconSize: [38, 38] // size of the icon
-      });
+    //     iconUrl: gpsIcon,
+    //     iconSize: [38, 38] // size of the icon
+    //   });
     return (
         <div className='max-w-7xl mx-auto rounded-md my-9 p-3'>
             <HelmetProvider>
@@ -29,13 +29,13 @@ const Contact = () => {
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
-  <Marker position={position} Icon={customIcon}>
+  <Marker position={position}>
       <Popup>
       1889 NW 79st St, Asheville, NC 98726
       </Popup>
     </Marker>
 </MapContainer>
-        <div className='flex flex-col lg:flex-row gap-6 font-roboto items-center justify-center shadow-md rounded p-14 w-full lg:w-[80%] mx-auto lg:-translate-y-8 z-40'>
+        <div className='flex flex-col lg:flex-row gap-6 font-roboto items-center justify-center shadow-md rounded p-14 w-full lg:w-[80%] mx-auto lg:-translate-y-8'>
             <div className='flex flex-col items-center space-y-3 text-xl'>
                 <FaMobile></FaMobile>
                 <p>11223 543 5432</p>
